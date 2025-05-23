3de46de5071044ef7937433cb8d750200a8a484c
Hi-insomnia <msn2204@yandex.ru>
2025-05-23 13:50:06 +0500

    simple project

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ mul = a * b ;
 console.log(mul);
 
 }
+
+{
+    // let photosInFolder, photosOnPage, numberOfPage;
+    // photosInFolder = parseInt(prompt("Введите количество фото в папке", 10));
+    // photosOnPage = parseInt(prompt("Введите количество фото на одной странице", 10));
+
+    // numberOfPage = Math.ceil(photosInFolder / photosOnPage);
+
+    // alert("количество страниц = " + numberOfPage);
+    
+}
+
+{
+    let a = 1,b;
+    b = a++;
+    console.log(a, b);
+}
+
+{
+    let a = 15 % 4; //остаток от деления
+    let b = Math.floor(14 % 5); //целочисленное деление
+    console.log(a, b);
+}
+
+{
+
+    let number, d1, d2, d3, d4;
+    number = parseInt(prompt("Введите 4-значное число"));
+    d1 = number % 10;
+    d2 = Math.floor(number/10) % 10;
+    d3 = Math.floor(number/100) % 10;
+    d4 = Math.floor(number/1000) % 10;
+    console.log(d1,d2,d3,d4);
+}
