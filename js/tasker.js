e5f8b8904f7572be398221aeee8df008e83285df
Hi-insomnia <msn2204@yandex.ru>
2025-05-29 13:01:35 +0500

    create tasker

--- js/tasker.js
+++ js/tasker.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const btnCreate = document.getElementById("btn-create-task");
 const newTask = document.getElementById("new-task");
-
+const buttons = document.querySelector(".buttons");
 let tasks = document.querySelectorAll(".task");
 
 
@@ -27,4 +27,49 @@ btnRemove.addEventListener("click", function(){
     tasks = document.querySelectorAll(".task");
     console.log(tasks);
 
+})
+
+//сортировка по алфавиту
+//let tasks = document.querySelectorAll(".task");
+//const container = document.querySelector(".container");
+const btnSort1 = document.getElementById("btn-sort1");
+function compare(a, b) {
+    if (a.querySelector("span").innerHTML > b.querySelector("span").innerHTML) return 1;
+
+    if (a.querySelector("span").innerHTML == b.querySelector("span").innerHTML) return 0;
+   
+    if (a.querySelector("span").innerHTML < b.querySelector("span").innerHTML) return -1;
+
+}
+
+btnSort1.addEventListener("click", function (){
+    let newTask = [...tasks].sort(compare)
+    for (let i in newTask) {
+        container.append(newTask[i])
+    }
+});
+
+
+//обратная сортировка
+const btnSort2 = document.getElementById("btn-sort2");
+function compareReverse(a, b) {
+    if (a.querySelector("span").innerHTML > b.querySelector("span").innerHTML) return -1;
+
+    if (a.querySelector("span").innerHTML == b.querySelector("span").innerHTML) return 0;
+   
+    if (a.querySelector("span").innerHTML < b.querySelector("span").innerHTML) return 1;
+
+}
+
+btnSort2.addEventListener("click", function (){
+    let newTask = [...tasks].sort(compareReverse)
+    for (let i in newTask) {
+        container.append(newTask[i])
+    }
+});
+
+const btnEmergency = document.getElementById("btn-emergency");
+btnEmergency.addEventListener("click", function(){
+    let newTask = [...tasks];
+    
 })
